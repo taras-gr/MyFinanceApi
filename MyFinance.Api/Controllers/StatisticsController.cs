892b9f29e2f889d257f92caeea59a6b78f2d3d84
taras-gr <tar.grytsenko@gmail.com>
2020-09-23 18:43:25 +0300

    Added statistics controller

---
+++ MyFinance.Api/Controllers/StatisticsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyFinance.Api.Helpers;
+using MyFinance.Services;
+using MyFinance.Services.Interfaces;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace MyFinance.Api.Controllers
+{
+    [ApiController]
+    [Route("api/users/{userName}/stats")]
+    [Authorize]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IStatisticsService _statisticsService;
+
+        public StatisticsController(IStatisticsService statisticsService)
+        {
+            _statisticsService = statisticsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable>> GetUserExpenseStats(
+            string userName, 
+            [FromQuery] DateTimeOffset startDate, 
+            DateTimeOffset endDate, 
+            ExpenseGroupByProperty statsByProperty)
+        {
+            var userIdFromToken = User.GetUserIdAsGuid();
+            string currentUserName = User.GetUserName();
+
+            if (currentUserName != userName)
+            {
+                return Unauthorized();
+            }
+
+            var dailyStats = await _statisticsService
+                .GetUserExpenseStatsByProperty(userIdFromToken, startDate, endDate, statsByProperty);
+
+            return Ok(dailyStats);
+        }
+    }
+}
